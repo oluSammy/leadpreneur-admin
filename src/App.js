cf99a44424195e55d84da8a9b4b1ed0a128afec6
oluSammy <olumorinsammy@gmail.com>
2020-08-31 22:41:16 -0700

    the journey begins

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.scss';
+import Dashboard from './pages/dashboard/dashboard.component';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello Leadpreneuer Admin</h1>
+      <Dashboard />
     </div>
   );
 }
