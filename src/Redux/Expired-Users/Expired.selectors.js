3f23429b3cebf85907dd9b3b6ded229f2653e828
oluSammy <olumorinsammy@gmail.com>
2020-10-13 14:43:46 -0700

    ..

--- src/Redux/Expired-Users/Expired.selectors.js
+++ src/Redux/Expired-Users/Expired.selectors.js
@@ -1,4 +1,4 @@
-import { createSelector } from 'reselect';
+    import { createSelector } from 'reselect';
 
 const selectExpiredUsers = state => state.expiredUsers;
 
--- src/utilityFunctions/timeStampToDate.js
+++ src/utilityFunctions/timeStampToDate.js
@@ -1,4 +1,6 @@
 export const timeStampToDate = dateObj => {
+    console.log(dateObj);
+    console.log(dateObj.toDate());
     const dateString = dateObj.toDate().toString().split(' ');
     return dateString.splice(0, 4).join(' ')
 }
